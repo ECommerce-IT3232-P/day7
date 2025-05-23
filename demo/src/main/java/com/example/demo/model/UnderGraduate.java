8151d88be1caa3af042fa2f5cbbddfa6c97eccbc
madushilakmini <139555956+madushilakmini@users.noreply.github.com>
2025-05-23 15:44:48 +0530

    feat: create UnderGraduate entity with many-to-many relationship to Session

---
+++ demo/src/main/java/com/example/demo/model/UnderGraduate.java
@@ -0,0 +1,29 @@
+package com.example.demo.model;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.JoinColumn;
+@Entity
+public class UnderGraduate {
+	 @Id
+	    private String id;
+
+	    private String name;
+	    private String email;
+	    private String gender;
+	    private String phone;
+	    private String degree;
+	    private String university;
+
+	    @ManyToMany
+	    @JoinTable(
+	        name = "under_graduate_sessions",
+	        joinColumns = @JoinColumn(name = "under_graduates_id"),
+	        inverseJoinColumns = @JoinColumn(name = "sessions_id")
+	    )
+	    private List<Session> sessions;
+}
